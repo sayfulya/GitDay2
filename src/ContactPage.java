94436b1fb57d86e2273f3fc5fe1950beba2dba09
sayfulya <sayfulya@gmail.com>
2022-12-23 08:59:22 -0500

    corrected contacts

--- src/ContactPage.java
+++ src/ContactPage.java
@@ -3,7 +3,9 @@ public class ContactPage {
     public static void main(String[] args) {
 
         //facebook info
-        //twitter info
+        //Twitter info
         //instagram info
+        //corrected
+
     }
 }

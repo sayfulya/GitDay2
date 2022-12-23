dec69e4d406e95e7339d9a072c92b73093e344ef
sayfulya <sayfulya@gmail.com>
2022-12-23 09:24:09 -0500

    TC for zoom out function

---
+++ src/loginFunction/ZoomOutFunction.java
@@ -0,0 +1,16 @@
+package loginFunction;
+
+public class ZoomOutFunction {
+
+    public static void main(String[] args) {
+
+
+        //testing zoom out function
+
+        //codes
+
+        //try catch blocks
+
+
+    }
+}
